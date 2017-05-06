2e6254d91cc10e6258e7e4d334da2fa34b741c8b
rmhorne <ryan@argos-2.local>
2017-05-06 17:39:52 -0400

    Added new scripts and expanded the readme

--- README.md
+++ README.md
@@ -1,2 +1,87 @@
-# mallet_utilities
-Utilities for parsing / examining output from Mallet (http://mallet.cs.umass.edu/index.php)
+# Mallet Utilities
+Utilities for parsing / examining output from [Mallet](http://mallet.cs.umass.edu/index.php)
+
+*These utilities require mallet-2.0.7*
+***
+
+The main script, `bamMallet.py`, runs analysis on a .mallet file with configuration options in `bamMalletConfig.json`, outputs the results into a specified directory structure, and creates some .csv files for use with Gephi, R, or other SNA applications.
+It does *not* import topics into the .mallet format, as you will almost certainly want to configure different stop words and other options.
+
+Once you have produced a .mallet file, you can invoke this utility by calling `bamMallet.py malletFileLocation bamMalletConfigFileName directoryLocation`
+
+## Script Parameters:
+1. `malletFileLocation`: The path to your .mallet file.
+
+2. `bamMalletConfigFileName`: the path to your .json configuration file
+
+3. `directoryLocation`: Where you want the output directory structure placed relative to the invoked script
+
+***
+## json Config File:
+The script reads a config file like the one outlined below:
+
+```javascript
+{
+	"malletInstallDirectory": "/Applications/mallet-2.0.7",
+	"topicCounts" :[
+		40,
+		60,
+		80,
+		100,
+		120,
+		160,
+		200,
+		240,
+		280
+	],
+	"optimizationIntervals":[
+		10,
+		50,
+		100,
+		500,
+		1000,
+		2000
+	],
+	"commands":
+	[
+		{
+			"command": "--output-state",
+			"output": "state.gz"
+		},
+		{
+			"command": "--output-topic-keys",
+			"output": "keys.txt"
+		},
+		{
+			"command": "--output-doc-topics",
+			"output": "topicsCompostion.txt"
+		},
+		{
+			"command": "--word-topic-counts-file",
+			"output": "topicCounts.txt"
+		}
+	]
+}
+```
+### json parameters:
+1. `malletInstallDirectory`: Where you installed Mallet
+
+2. `topicCounts`: A list of the number of topic counts you want to run on your corpus
+
+3. `optimizationIntervals`: A list of the different optimization intervals you want to run for each of your topic counts
+
+4. `commands`: A list of Mallet commands that you want to run on each combination of topic count and optimization variables. Each command is specified by the `command` value, the output file name is specified by the `output` value. For a list of commands, see [http://mallet.cs.umass.edu/topics.php](http://mallet.cs.umass.edu/topics.php)
+
+***
+## Output
+
+1. The script outputs your results into a directory structure that looks like this: `directory/topicCount/optimizationValue`, where `directory` is the location you specified in the command line, `topicCount` is one of the number of topics choices taken from you config file, and `optimizationValue` is an optimization interval, also taken from your config file.
+
+2. Depending on your choice of commands in the config file, you will have a number of files in each directory. Using the "default" config, you will see the following:
+ * `keys_[Edges].csv`: A .csv with `source` and `target` columns created from the `--output-topic-keys` mallet option. The `source` column is the topic number, while the `target` column is an individual word.
+ * `keys.txt`: Standard output from the `--output-topic-keys` mallet option.
+ * `state.gz`: Standard output from the `--output-state` mallet option.
+ * `topicCounts_All[Edges].csv`: A .csv with `source`,`target`, and `frequency` columns created from the `--word-topic-counts-file` mallet option. The `source` column is the topic number, while the `target` column is an individual word, and the `frequency` column is number of times the word is assigned to the topic in the corpus. This .csv file lists *all* words displayed as a result of the `--word-topic-counts-file` mallet option.
+ * `topicCounts_Shared[Edges].csv`: Exactly the same as the `topicCounts_All[Edges].csv` file, except *only* words that appear in *more than one* topic are listed.
+ * `topicCounts.txt`: Standard output from the `--word-topic-counts-file` mallet option.
+ * `topicsCompostion.txt`: Standard output from the `--output-doc-topics` mallet option.
---
+++ bamMallet.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# copyright 2017 Ryan Horne, Released under GPLv3
+# Version 0.9
+# utility to execute mallet given a .mallet file on the command line
+# the command is: python bamMallet.py malletFile bamMalletConfig outputDirectory
+# malletFile = path to your .mallet file
+# bamMalletConfig = path to the config.json file
+# outputDirectory = path to the output directory you want
+# right now there is no graceful error checking - it just blows up if there is a problem
+#
+
+import csv 
+import sys
+import os
+import json
+import subprocess
+
+
+#check to see that we have arguments
+if len(sys.argv) > 0:
+	#assign the values as needed
+	malletFileName = sys.argv[1]
+	configFileName = sys.argv[2]
+	outputDirectoryName = sys.argv[3]
+		
+	# make the "holder" directory if it is not there. We will overwrite any files in it that were made before
+	if not os.path.exists(outputDirectoryName):
+		os.makedirs(outputDirectoryName)
+		
+	with open(configFileName, 'r') as json_data:
+		data = json.load(json_data)
+		
+		mainCommand = ('{0}/./bin/mallet train-topics --input {1}'.format(data["malletInstallDirectory"],malletFileName))
+
+		for topicCount in data["topicCounts"]:
+			pathForTopic = ('{0}/Topic_count_{1}'.format(outputDirectoryName, topicCount))
+			if not os.path.exists(pathForTopic):
+				os.makedirs(pathForTopic)
+			
+			for optNumber in data["optimizationIntervals"]:
+				tempCommand = ''
+				tempCommand +=(' --num-topics {0}'.format(topicCount))
+				tempCommand +=(' --optimize-interval {0}'.format(optNumber))
+				pathForOpt =  ('{0}/Optimization_count_{1}'.format(pathForTopic, optNumber))
+				if not os.path.exists(pathForOpt):
+					os.makedirs(pathForOpt)
+				#now fire the commands
+				for commandText in data["commands"]:
+					tempCommand += (' {0} {1}/{2}'.format(commandText["command"], pathForOpt, commandText["output"]))
+				
+				executeCommand = ('{0}{1}'.format(mainCommand,tempCommand))
+				subprocess.call(executeCommand, shell=True)
+				
+				#Now take these outputs and make what files we need off of them - Gephi input, etc
+				commandText =''
+				for commandText in data["commands"]:
+					#check if the command wants Gephi files and what command it is
+					if commandText["csv"]=="gephi" and commandText["command"] == "--output-topic-keys":
+						outCommandName = commandText["output"].split('.')
+						outputFileName = ('{0}/{1}_[Edges].csv'.format(pathForOpt,outCommandName[0]))
+						outputFile = open(outputFileName, 'w')
+						outputFile.writelines ("source,target")
+						outputFile.writelines('\n')
+						with open('{0}/{1}'.format(pathForOpt,commandText["output"]), 'rb') as tsv:
+							for line in csv.reader(tsv, delimiter="\t"):
+								# split the text
+								words = line[2].split()
+								# for each word in the line:
+								for word in words:
+									outputFile.writelines('{0},{1}'.format(line[0],word))
+									outputFile.writelines('\n')
+							#close all the files
+							outputFile.close()
+							tsv.close()
+					
+					if commandText["csv"]=="gephi" and commandText["command"] == "--word-topic-counts-file":
+						outCommandName = commandText["output"].split('.')
+						allOutputFileName = ('{0}/{1}_All[Edges].csv'.format(pathForOpt,outCommandName[0]))
+						allOutputFile = open(allOutputFileName, 'w')
+						allOutputFile.writelines ("source,target,frequency")
+						sharedConnectionsOutputFileName = ('{0}/{1}_Shared[Edges].csv'.format(pathForOpt,outCommandName[0]))
+						sharedConnectionsOutputFile = open(sharedConnectionsOutputFileName, 'w')
+						sharedConnectionsOutputFile.writelines ("source,target,frequency")
+						
+						with open('{0}/{1}'.format(pathForOpt,commandText["output"]), 'rb') as frequencyFile:
+							for line in csv.reader(frequencyFile, delimiter=" "):
+								#first, add the first entry to the all nodes file. Next we add anything beyond that to the connections only file
+									for i in range(2, len(line)):
+										allOutputFile.writelines('\n')
+										allOutputFile.writelines('{0},"{1}",{2}'.format(line[i].split(':')[0],line[1],line[i].split(':')[1]))
+										
+										if len(line) > 3:
+											sharedConnectionsOutputFile.writelines('\n')
+											sharedConnectionsOutputFile.writelines('{0},"{1}",{2}'.format(line[i].split(':')[0],line[1],line[i].split(':')[1]))
+											
+allOutputFile.close()
+sharedConnectionsOutputFile.close()
+frequencyFile.close()
